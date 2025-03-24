bf4589b07772c2a6543f85b91c98c5710af96ee7
Soni Rawat <soni.r@srmtechsol.com>
2025-03-24 18:17:18 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating= {5}/>
+    <StarRating maxRating= {5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}/>
+    <StarRating size= {24} color='red' className="test"/>
     {/* <StarRating maxRating= {10}/>
     <StarRating /> */}
   </React.StrictMode>
